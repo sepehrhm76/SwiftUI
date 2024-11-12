443a1485cd94a4be836f8acfe355d98df3bc6bc6
sepehrhm <s.hajimohammadi@nizek.com>
2024-11-12 12:47:24 +0330

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Sepehr on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
